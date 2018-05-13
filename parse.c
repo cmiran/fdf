5aab8016f0a42af2bfeeaf4b3a369dbd3eea09fc
cmiran <cmiran@student.42.fr>
2018-05-13 19:01:06 +0200

    refaire le parser... ft_strplit !

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 15:57:46 by cmiran            #+#    #+#             */
-/*   Updated: 2018/05/11 18:55:28 by cmiran           ###   ########.fr       */
+/*   Updated: 2018/05/13 18:08:26 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,15 @@ int		line_len(char *line)
 	while (line[i])
 	{
 		if (ft_isdigit(line[i]))
-		{
+ 		{
 			len++;
-			while (ft_isgraph(line[i]))
+			while (ft_isgraph(line[i]) && line[i])
 				i++;
 		}
+		if (line[i] == '\0')
+			break;
 		i++;
 	}
-	printf("%s\n", line);
-	printf("len : %d\n", len);
-	printf("strlen : %ld\n", strlen(line));
 	return (len);
 }
 
@@ -43,49 +42,44 @@ void	check_point(char *line)
 	i = 0;
 	while (line[i])
 	{
-		if (!ft_isdigit(line[i]) && line[i] != ' ')
-			kill("Error : map contains a forbidden character");
 		if (line[i] == ',')
 		{
-			i++;
-			if (line[i] != '0' && line[i + 1] != 'x')
+			if (line[i + 1] != '0' && line[i + 2] != 'x')
 				kill("Error : a color is not well formated");
 			else
-				i += 2;
+				i += 3;
 			j = 1;
 			while (line[i] != ' ')
 			{
-				if (!ft_isxdigit(line[i]) && j > 6)
+				if (!ft_isxdigit(line[i]) && (j > 6 || j % 2 != 0))
 					kill("Error : a color is not well formated");
 				i++;
 				j++;
 			}
 		}
+		if (!ft_isdigit(line[i]) && line[i] != ' ')
+			kill("Error : map contains a forbidden character");
 		i++;
 	}
 }
 
-void		check_line(char *line, int *nb_x)
+void	check_line(char *line, int *nb_x)
 {
-	if (!ft_isdigit(line[0]))
+	printf("%s\n", line);
+	if (!ft_isdigit(line[0]) || !ft_isxdigit(line[ft_strlen(line) - 1]))
 		kill("Error : map is not well formated or contains a forbidden character");
 	else
 		check_point(line);
 	if (!*nb_x)
 		*nb_x = line_len(line);
 	else if (*nb_x != line_len(line))
-	{
-		printf("nb_x : %d\n", *nb_x);
 		kill("Error : map is not a square or a rectangle");
-	}
 }
 
 void 	check_map(const int fd, int *nb_x, int *nb_y)
 {
-	int					i;
 	char				*line;
 
-	i = 0;
 	*nb_y = 0;
 	while (get_next_line(fd, &line) != 0)
 	{
