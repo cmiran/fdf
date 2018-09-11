cad4958cd85a9084ef54ec89cdc9528263ab7c76
cmiran <cmiran@student.42.fr>
2018-09-11 12:52:42 +0200

    review

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,14 +6,14 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 15:32:18 by cmiran            #+#    #+#             */
-/*   Updated: 2018/09/06 19:28:29 by cmiran           ###   ########.fr       */
+/*   Updated: 2018/09/11 12:52:11 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "../libft/libft.h"
+# include "../libft/includes/libft.h"
 
 typedef struct s_point
 {
